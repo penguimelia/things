fbbc8bb91b9edd4bf8a3b34ef771b2298ff4ff6b
Huang <amelia.h.huang@accenture.com>
2017-07-18 11:28:20 +0100

    Updated index, css and js files to use Bower dependencies

--- js/application.js
+++ js/application.js
@@ -23,39 +23,68 @@ $(document).ready(function () {
     }
   });
 
-
-  $('.slidey').slick()
+  $('.slidey').slick();
 })
 
 //google maps
-function initMap() {
-  var london = {lat: 51.5074, lng: -0.1278};
-  var map = new google.maps.Map(document.getElementById('map'), {
+var address;
+var map;
+var london = {lat: 51.5074, lng: -0.1278};
+
+$(window)["0"].GoogleMapsLoader.KEY="AIzaSyApejepoIEkCMu8QL67kOsPcKrQ1hCphn8";
+$(window)["0"].GoogleMapsLoader.WINDOW_CALLBACK_NAME= "initMap";
+$(window)["0"].GoogleMapsLoader.onLoad(function(google) {
+	console.log('loaded google maps api');
+});
+
+GoogleMapsLoader.load(function(google) {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
     center: london,
     scrollwheel: false
   });
-  var marker = new google.maps.Marker({
-    position: london,
-    map: map
+});
+
+function geocodeAddress(geocoder, resultsMap) {
+
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      bootbox.alert('Geocode was not successful for the following reason: ' + status);
+    }
   });
 }
 
 //yahoo weather api
 function getWeather() {
-    var location = $('#location').val();
 
-    $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function (data) {
-        /* Check that a place was found (we'll just grab the first) */
-        if (data.query.results === null) {
-            bootbox.alert("Location not found: " + location + "!");
+  $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + address + '")&format=json', function (data) {
+      /* Check that a place was found (we'll just grab the first) */
+    if (data.query.results === null) {
+      bootbox.alert("Location not found: " + address + "!");
 
-        } else {
-          var desc = data.query.results.channel.item.description;
-          var final = desc.replace("<![CDATA[",  "").replace("]]>", "");
-            $('.results').show().html('<h2>' + data.query.results.channel.item.title + '</h2>' +
-                final);
-        }
+    } else {
+      var desc = data.query.results.channel.item.description;
+      var final = desc.replace("<![CDATA[",  "").replace("]]>", "");
+      $('#results').show().html('<h3>' + data.query.results.channel.item.title + '</h3>' +
+          final);
+    }
+  });
+}
 
-    });
+function statusSubmit() {
+  address = $('#location').val();
+
+  $('#map').show();
+  google.maps.event.trigger(map, 'resize');
+
+  var geocoder = new google.maps.Geocoder;
+  geocodeAddress(geocoder, map);
+
+  getWeather();
 }
